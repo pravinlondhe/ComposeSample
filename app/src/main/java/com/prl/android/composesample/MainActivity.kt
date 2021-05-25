9d18f63f6a58bcef0a881781b96f713b82b1d927
Pravin Londhe <pravinlondhe15@gmail.com>
2021-05-25 10:10:29 +0530

    row and column

--- app/src/main/java/com/prl/android/composesample/MainActivity.kt
+++ app/src/main/java/com/prl/android/composesample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
@@ -12,11 +13,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,27 +38,29 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun NewsStory() {
-        Column(
+        Row(
             modifier = Modifier
                 .padding(Dp(8f))
-                .fillMaxWidth()
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ima), null,
-                contentScale = ContentScale.Inside, modifier = Modifier
-                    .size(120.dp, 120.dp)
+                painter = painterResource(id = R.drawable.facepaint),
+                null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(120.dp, 120.dp)
                     .clip(
                         CircleShape
-                    ),
-                alignment = Alignment.Center
+                    )
             )
-            Spacer(Modifier.height(16.dp))
-            Row {
-                Text("heading")
-                Text("Body", modifier = Modifier.padding(start = 8.dp))
+            Spacer(Modifier.size(16.dp))
+            Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceEvenly) {
+                Text("heading",fontSize = 18.sp, )
+                Text("Body", modifier = Modifier.padding(top = 8.dp, bottom = 8.dp), fontSize = 14.sp)
+                Text("Message Message Message \n Message Message Message Message")
+                Text("End lines End lines End lines")
             }
-            Text("Message Message Message \n Message Message Message Message")
-            Text("End lines End lines End lines")
         }
     }
 
